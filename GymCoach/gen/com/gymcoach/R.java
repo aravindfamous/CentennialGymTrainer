7ae874b1ed8591db3096596eb8458240b8db3782
Romaine Herrera <pizza_aldwyn@yahoo.com>
2014-02-28 16:42:49 -0500

    Cleaning the project

--- GymCoach/gen/com/gymcoach/R.java
+++ GymCoach/gen/com/gymcoach/R.java
@@ -8,6 +8,11 @@
 package com.gymcoach;
 
 public final class R {
+    public static final class array {
+        /**  Nav Drawer Menu Items 
+         */
+        public static final int nav_drawer_items=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -22,84 +27,179 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int custom_button=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int trainer=0x7f020003;
+        public static final int border=0x7f020001;
+        public static final int custom_button=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int leftbg=0x7f020004;
+        public static final int trainer=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080023;
-        public static final int btnLinkToLoginScreen=0x7f080021;
-        public static final int btnLinkToRegisterScreen=0x7f080006;
-        public static final int btnLogin=0x7f080004;
-        public static final int btnLogout=0x7f080000;
-        public static final int btnRegister=0x7f080020;
-        public static final int etAddress=0x7f080018;
-        public static final int etCPassword=0x7f08001d;
-        public static final int etCity=0x7f080019;
-        public static final int etDOB=0x7f08001c;
-        public static final int etEmail=0x7f08001e;
-        public static final int etFirstName=0x7f080016;
-        public static final int etLastName=0x7f080017;
-        public static final int etPassword=0x7f080005;
-        public static final int etPostalCode=0x7f08001b;
-        public static final int etProvince=0x7f08001a;
-        public static final int etUsername=0x7f080003;
-        public static final int imageView1=0x7f080001;
-        public static final int spinnerUserType=0x7f080022;
-        public static final int textView1=0x7f080002;
-        public static final int textView2=0x7f080008;
-        public static final int tvAddress=0x7f08000b;
-        public static final int tvCPassword=0x7f080013;
-        public static final int tvCity=0x7f08000c;
-        public static final int tvDOB=0x7f08000f;
-        public static final int tvEmail=0x7f080014;
-        public static final int tvFirstName=0x7f080009;
-        public static final int tvLastName=0x7f08000a;
-        public static final int tvLoginError=0x7f080007;
-        public static final int tvPassword=0x7f080012;
-        public static final int tvPostalCode=0x7f08000e;
-        public static final int tvProvince=0x7f08000d;
-        public static final int tvRegisterError=0x7f08001f;
-        public static final int tvRegistration=0x7f080015;
-        public static final int tvUserType=0x7f080010;
-        public static final int tvUsername=0x7f080011;
+        public static final int Home=0x7f09000d;
+        public static final int action_settings=0x7f090046;
+        public static final int btnBehindLogout=0x7f090002;
+        public static final int btnCalculateBMIandBMR=0x7f090023;
+        public static final int btnChangeEmail=0x7f09000a;
+        public static final int btnDone=0x7f090005;
+        public static final int btnGenerateAPlan=0x7f090007;
+        public static final int btnLinkToLoginScreen=0x7f090042;
+        public static final int btnLinkToRegisterScreen=0x7f090012;
+        public static final int btnLogin=0x7f090014;
+        public static final int btnRegister=0x7f090041;
+        public static final int btnSendInfo=0x7f090027;
+        public static final int btnSendVerification=0x7f090009;
+        public static final int checkBox1=0x7f09000e;
+        public static final int code=0x7f09000f;
+        public static final int etAddress=0x7f09003c;
+        public static final int etAge=0x7f09001d;
+        public static final int etCPassword=0x7f090045;
+        public static final int etCity=0x7f09003b;
+        public static final int etDOB=0x7f09003e;
+        public static final int etEmail=0x7f09000b;
+        public static final int etFirstName=0x7f090039;
+        public static final int etHeightFeet=0x7f09001e;
+        public static final int etHeightInches=0x7f090020;
+        public static final int etLastName=0x7f09003a;
+        public static final int etPassword=0x7f090013;
+        public static final int etPostalCode=0x7f09003f;
+        public static final int etProvince=0x7f09003d;
+        public static final int etUsername=0x7f090016;
+        public static final int etWeight=0x7f09001f;
+        public static final int imageView1=0x7f090011;
+        public static final int listView1=0x7f090004;
+        public static final int numbers=0x7f090010;
+        public static final int radioFemale=0x7f09002a;
+        public static final int radioGender=0x7f090028;
+        public static final int radioMale=0x7f090029;
+        public static final int spinnerUserType=0x7f090043;
+        public static final int spinnerWorkoutType=0x7f09002c;
+        public static final int textView1=0x7f090008;
+        public static final int textView2=0x7f090015;
+        public static final int textView3=0x7f090021;
+        public static final int tvAddress=0x7f09002f;
+        public static final int tvAge=0x7f090019;
+        public static final int tvBMI=0x7f090022;
+        public static final int tvBMR=0x7f090024;
+        public static final int tvCPassword=0x7f090037;
+        public static final int tvCity=0x7f090030;
+        public static final int tvCurrentDay=0x7f090003;
+        public static final int tvDOB=0x7f090033;
+        public static final int tvDisplayBMI=0x7f090026;
+        public static final int tvDisplayBMR=0x7f090025;
+        public static final int tvEmail=0x7f090038;
+        public static final int tvExerciseCurrentDay=0x7f090001;
+        public static final int tvFirstName=0x7f09002d;
+        public static final int tvGender=0x7f09001c;
+        public static final int tvHeight=0x7f09001a;
+        public static final int tvHome=0x7f090000;
+        public static final int tvLastName=0x7f09002e;
+        public static final int tvLoginError=0x7f090017;
+        public static final int tvPassword=0x7f090036;
+        public static final int tvPersonalInfo=0x7f090018;
+        public static final int tvPostalCode=0x7f090032;
+        public static final int tvProvince=0x7f090031;
+        public static final int tvRegisterError=0x7f090040;
+        public static final int tvRegistration=0x7f090044;
+        public static final int tvUserType=0x7f090034;
+        public static final int tvUsername=0x7f090035;
+        public static final int tvVerified=0x7f09000c;
+        public static final int tvWeight=0x7f09001b;
+        public static final int tvWelcome=0x7f090006;
+        public static final int tvWorkoutType=0x7f09002b;
     }
     public static final class layout {
-        public static final int dashboard=0x7f030000;
-        public static final int login=0x7f030001;
-        public static final int register=0x7f030002;
+        public static final int activity_behind_left_simple=0x7f030000;
+        public static final int activity_exercise=0x7f030001;
+        public static final int dashboard=0x7f030002;
+        public static final int dashboardnotverified=0x7f030003;
+        public static final int dashboardverified=0x7f030004;
+        public static final int listview_layout=0x7f030005;
+        public static final int login=0x7f030006;
+        public static final int personalinforegister=0x7f030007;
+        public static final int register=0x7f030008;
+        public static final int verifyyouremail=0x7f030009;
     }
     public static final class menu {
-        public static final int login=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int register=0x7f070002;
+        public static final int exercise=0x7f080000;
+        public static final int login=0x7f080001;
+        public static final int main=0x7f080002;
+        public static final int register=0x7f080003;
+        public static final int verify=0x7f080004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int address=0x7f050011;
+        public static final int age=0x7f050023;
         public static final int app_name=0x7f050000;
+        public static final int bmi=0x7f050027;
+        public static final int bmr=0x7f050028;
+        public static final int calculatebmiandbmr=0x7f05002e;
+        public static final int changeemail=0x7f050039;
         public static final int city=0x7f050012;
         public static final int confirmpassword=0x7f050009;
         public static final int dateformat=0x7f050019;
+        /**  Nav Drawer List Item Icons 
+
+    Keep them in order as the titles are in
+    <array name="nav_drawer_icons">
+        <item>@drawable/ic_home</item>
+        <item>@drawable/ic_people</item>
+        <item>@drawable/ic_photos</item>
+        <item>@drawable/ic_communities</item>
+        <item>@drawable/ic_pages</item>
+        <item>@drawable/ic_whats_hot</item>
+    </array>
+
+
+
+    
+ Content Description 
+         */
+        public static final int desc_list_item_icon=0x7f05001e;
         public static final int dob=0x7f050017;
+        public static final int done=0x7f050036;
+        public static final int drawer_close=0x7f05001d;
+        public static final int drawer_open=0x7f05001c;
         public static final int email=0x7f050016;
+        public static final int exerciseplan=0x7f050033;
+        public static final int female=0x7f05002d;
         public static final int firstname=0x7f05000f;
-        public static final int hello_world=0x7f050005;
+        public static final int ft=0x7f05002a;
+        public static final int gender=0x7f050026;
+        public static final int generateaplan=0x7f05001b;
+        public static final int gymcentennialtrainer=0x7f05001f;
+        public static final int height=0x7f050024;
+        public static final int hello_world=0x7f050021;
+        public static final int home=0x7f050032;
+        public static final int in=0x7f05002b;
         public static final int lastname=0x7f050010;
         public static final int linktologin=0x7f05000e;
         public static final int linktoregister=0x7f05000d;
         public static final int login=0x7f05000c;
         public static final int logo=0x7f05000a;
         public static final int logout=0x7f050002;
+        public static final int male=0x7f05002c;
+        public static final int notverified=0x7f050037;
         public static final int password=0x7f050008;
+        public static final int pds=0x7f050029;
+        public static final int personalinfo=0x7f050022;
         public static final int postalcode=0x7f050014;
         public static final int province=0x7f050013;
         public static final int register=0x7f05000b;
         public static final int registration=0x7f050018;
+        public static final int sendinfo=0x7f05002f;
+        public static final int sendverification=0x7f050038;
+        public static final int title_activity_exercise=0x7f050034;
         public static final int title_activity_login=0x7f050004;
-        public static final int title_activity_register=0x7f050006;
+        public static final int title_activity_main=0x7f050020;
+        public static final int title_activity_register=0x7f050005;
+        public static final int title_activity_simple=0x7f050006;
+        public static final int title_activity_verify=0x7f050031;
+        public static final int typeofworkout=0x7f050035;
         public static final int username=0x7f050007;
         public static final int usertype=0x7f050015;
+        public static final int verified=0x7f05001a;
+        public static final int verify=0x7f050030;
+        public static final int weight=0x7f050025;
         public static final int welcome=0x7f050003;
     }
     public static final class style {
@@ -124,11 +224,19 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070006;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
-        public static final int btnBlue=0x7f060002;
+        public static final int AppTheme=0x7f070007;
+        public static final int BlackFontTheme=0x7f07000a;
+        public static final int behindMenuButton=0x7f070004;
+        public static final int behindMenuItemLabel=0x7f070003;
+        public static final int behindMenuItemTitle=0x7f070002;
+        public static final int behindMenuScrollContent=0x7f070001;
+        public static final int btnBlue=0x7f070008;
+        public static final int hr=0x7f070005;
+        public static final int leftBehindMenuScroll=0x7f070000;
+        public static final int piStyle=0x7f070009;
     }
 }
--- GymCoach/src/com/library/DBHandler.java
+++ GymCoach/src/com/library/DBHandler.java
@@ -1,5 +1,6 @@
 package com.library;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import android.content.ContentValues;
@@ -19,13 +20,26 @@ public class DBHandler extends SQLiteOpenHelper {
  
     // Login table name
     private static final String TABLE_LOGIN = "login";
+    private static final String TABLE_EXERCISE = "exercise";
  
     // Login Table Columns names
-    private static final String KEY_ID = "gymuserID";
+    private static final String KEY_USERID = "gymuserID";
     private static final String KEY_FIRSTNAME = "firstname";
     private static final String KEY_LASTNAME = "lastname";
     private static final String KEY_USERNAME = "username";
     private static final String KEY_EMAIL = "email";
+    private static final String KEY_VERIFIED = "verified";
+    private static final String KEY_PLANID = "planID";
+    private static final String KEY_CURRENTDAY = "currentday";
+    
+    //Exercise Plan Table Column Names
+    private static final String KEY_EXERCISEID = "exerciseID";
+    private static final String KEY_BODYPART = "body_part";
+    private static final String KEY_EXERCISENAME = "exercise_name";
+    private static final String KEY_NUMOFREPS = "num_of_reps";
+    private static final String KEY_NUMOFSETS = "num_of_sets";
+    private static final String KEY_DAY = "day";
+    private static final String KEY_WORKOUTVIDEO = "workout_video";
  
     public DBHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -35,12 +49,28 @@ public class DBHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String CREATE_LOGIN_TABLE = "CREATE TABLE " + TABLE_LOGIN + "("
-                + KEY_ID + " INTEGER PRIMARY KEY,"
+                + KEY_USERID + " INTEGER PRIMARY KEY,"
                 + KEY_FIRSTNAME + " TEXT,"
                 + KEY_LASTNAME + " TEXT,"
                 + KEY_USERNAME + " TEXT,"
-                + KEY_EMAIL + " TEXT UNIQUE" + ")";
+                + KEY_EMAIL + " TEXT UNIQUE,"
+                + KEY_VERIFIED + " TEXT,"
+                + KEY_PLANID + " INTEGER,"
+                + KEY_CURRENTDAY + " INTEGER" 
+                + ")";
+        
+        String CREATE_EXERCISE_TABLE = "CREATE TABLE " + TABLE_EXERCISE + "("
+        		+ KEY_EXERCISEID + " INTEGER PRIMARY KEY,"
+        		+ KEY_BODYPART + " TEXT,"
+        		+ KEY_EXERCISENAME + " TEXT,"
+        		+ KEY_NUMOFREPS + " INTEGER,"
+        		+ KEY_NUMOFSETS + " INTEGER,"
+        		+ KEY_DAY + " INTEGER,"
+        		+ KEY_WORKOUTVIDEO + " TEXT"
+        		+ ")";
+        
         db.execSQL(CREATE_LOGIN_TABLE);
+        db.execSQL(CREATE_EXERCISE_TABLE);
     }
  
     // Upgrading database
@@ -48,7 +78,7 @@ public class DBHandler extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Drop older table if existed
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_LOGIN);
- 
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EXERCISE);
         // Create tables again
         onCreate(db);
     }
@@ -56,20 +86,39 @@ public class DBHandler extends SQLiteOpenHelper {
     /**
      * Storing user details in database
      * */
-    public void addUser(String firstname, String lastname, String username, String email) {
+    public void addUser(String firstname, String lastname, String username, String email, String verified, int planID, int currentday) {
         SQLiteDatabase db = this.getWritableDatabase();
  
         ContentValues values = new ContentValues();
         values.put(KEY_FIRSTNAME, firstname); // Name
         values.put(KEY_LASTNAME, lastname);
         values.put(KEY_USERNAME, username);
-        values.put(KEY_EMAIL, email); // Email
+        values.put(KEY_EMAIL, email);
+        values.put(KEY_VERIFIED, verified);
+        values.put(KEY_PLANID, planID);
+        values.put(KEY_CURRENTDAY, currentday);
  
         // Inserting Row
         db.insert(TABLE_LOGIN, null, values);
         db.close(); // Closing database connection
     }
      
+    
+    public void addExercisePlan(String bodyPart, String exerciseName, int numOfReps, int numOfSets, int day, String workoutVideo) {
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	 
+        ContentValues values = new ContentValues();
+        values.put(KEY_BODYPART, bodyPart); // Name
+        values.put(KEY_EXERCISENAME, exerciseName);
+        values.put(KEY_NUMOFREPS, numOfReps);
+        values.put(KEY_NUMOFSETS, numOfSets);
+        values.put(KEY_DAY, day);
+        values.put(KEY_WORKOUTVIDEO, workoutVideo);
+ 
+        // Inserting Row
+        db.insert(TABLE_EXERCISE, null, values);
+        db.close();
+    }
     /**
      * Getting user data from database
      * */
@@ -86,13 +135,41 @@ public class DBHandler extends SQLiteOpenHelper {
             user.put("lastname", cursor.getString(2));
             user.put("username", cursor.getString(3));
             user.put("email", cursor.getString(4));
+            user.put("verified", cursor.getString(5));
+            user.put("planID", cursor.getString(6));
+            user.put("currentday", cursor.getString(7));
         }
         cursor.close();
         db.close();
         // return user
         return user;
     }
- 
+    
+    public ArrayList<ExercisePlan> getExercisePlanByDay(int day) {
+    	ArrayList<ExercisePlan> exerciseList = new ArrayList<ExercisePlan>();
+    	String query = "SELECT * FROM " + TABLE_EXERCISE + " WHERE day=" + day;
+    	
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	Cursor cursor = db.rawQuery(query, null);
+    	
+    	if (cursor.moveToFirst()) {
+            do {
+            	ExercisePlan ep = new ExercisePlan();
+            	ep.setBodyPart(cursor.getString(1));
+            	ep.setExerciseName(cursor.getString(2));
+            	ep.setNumOfReps(cursor.getInt(3));
+            	ep.setNumOfSets(cursor.getInt(4));
+            	ep.setDay(cursor.getInt(5));
+            	ep.setWorkoutVideo(cursor.getString(6));
+            	ep.setSelected(false);
+                // Adding contact to list
+            	exerciseList.add(ep);
+            } while (cursor.moveToNext());
+        }
+    	
+    	return exerciseList;
+    }
+    
     /**
      * Getting user login status
      * return true if rows are there in table
@@ -108,6 +185,21 @@ public class DBHandler extends SQLiteOpenHelper {
         // return row count
         return rowCount;
     }
+    
+    public void updateCurrentDay(int day) {
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	ContentValues cv = new ContentValues();
+    	cv.put("currentday", day);
+    	db.update(TABLE_LOGIN, cv, "gymuserID='1'", null);
+    }
+    
+    public void updatePlanID(int planID) {
+    	SQLiteDatabase db = this.getWritableDatabase();
+    	ContentValues cv = new ContentValues();
+    	cv.put("planID", planID);
+    	db.update(TABLE_LOGIN, cv, "gymuserID='1'", null);
+    }
+
      
     /**
      * Re crate database
@@ -117,7 +209,7 @@ public class DBHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         // Delete All Rows
         db.delete(TABLE_LOGIN, null, null);
+        db.delete(TABLE_EXERCISE, null, null);
         db.close();
     }
-
 }
