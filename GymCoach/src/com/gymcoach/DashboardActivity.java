b52816c34c16cf0a7bc13fa5d0925988f8effb61
aravindfamous <aravindfamous@gmail.com>
2014-03-19 02:08:14 -0400

    Login and Left Menu UI Update

--- GymCoach/src/com/gymcoach/DashboardActivity.java
+++ GymCoach/src/com/gymcoach/DashboardActivity.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.library.BReceiver;
 import com.library.SimpleSideDrawer;
